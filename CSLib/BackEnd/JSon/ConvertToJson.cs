71daa06a18e2bac07956eb802bd6f99af8d9bee4
home_gaoz <nnugaozhi@hotmail.com>
2018-12-10 23:22:24 +0800

    通用数据库帮助类，DataTable转Json串

---
+++ CSLib/BackEnd/JSon/ConvertToJson.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLib.BackEnd.JSon
+{
+    public class ConvertToJson
+    {
+
+        public static String FromDataTable(DataTable pDT)
+        {
+            String lJsonStr = "";
+            if (pDT != null && pDT.Rows.Count > 0)
+            {
+                lJsonStr = "[";
+                foreach (DataRow lDR in pDT.Rows)
+                {
+                    lJsonStr += "{";
+                    foreach (DataColumn lDC in pDT.Columns)
+                    {
+                        lJsonStr += "\"" + lDC.ColumnName + "\":\"" + lDR[lDC.ColumnName] + "\",";
+                    }
+                    if (lJsonStr.EndsWith(","))
+                    {
+                        lJsonStr = lJsonStr.Substring(0, lJsonStr.Length - 1);
+                    }
+                    lJsonStr += "},";
+                }
+
+                if (lJsonStr.EndsWith(","))
+                {
+                    lJsonStr = lJsonStr.Substring(0, lJsonStr.Length - 1);
+                }
+                lJsonStr += "]";
+            }
+            return lJsonStr;
+        }
+    }
+}
---
+++ WebApplication/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using CSLib;
+using CSLib.BackEnd.JSon;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public String GetListData()
+        {
+            String lDataInJsonStr = "";
+            String lSQL = "";
+            lSQL += "SELECT TOP(10)";
+            lSQL += "       ID";
+            lSQL += "     , CustomerCode";
+            lSQL += "     , DistributorCode";
+            lSQL += "     , SubmitDate";
+            lSQL += "     , SubmitMan";
+            lSQL += "     , RecAddress";
+            lSQL += "     , DeliveryDate";
+            lSQL += "     , Money";
+            lSQL += "     , InvoiceMoney";
+            lSQL += " FROM";
+            lSQL += "       T_Order";
+            DataTable lDT = null;
+            if (DBHelper.GetDataTable(lSQL, ref lDT) == EXESQLRET.SUCCESS)
+            {
+                lDataInJsonStr = ConvertToJson.FromDataTable(lDT);
+            }
+            return lDataInJsonStr;
+        }
+    }
+}
