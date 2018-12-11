2431062204567e2af73f2c677dcc39b75e061879
home_gaoz <nnugaozhi@hotmail.com>
2018-12-11 21:45:54 +0800

    添加Ajax通用函数MyAjax

---
+++ WebApplication/Scripts/MyJsLib/MyJsLib.js
@@ -0,0 +1,21 @@
+﻿function MyAjax(pAjaxParam) {
+    $.ajax({
+        "type": "get",
+        "timeout": "6000",
+        "url": pAjaxParam.url,
+        "data": pAjaxParam.data,
+        "success": function (pData, pStatusText) {
+            alert(pData);
+            alert(pStatusText);
+            pAjaxParam.fnSuccess();
+        },
+        "error": function (pXHR, pStatusText) {
+            alert(pXHR);
+            alert(pStatusText);
+        },
+        "complete": function (pXHR, pStatusText) {
+            alert(pXHR);
+            alert(pStatusText);
+        }
+    });
+}
