ddf1606bde347f97e09a903f8b3f7a6cdacde56c
ntchst_gaoz <nnugaozhi@hotmail.com>
2018-12-28 17:49:32 +0800

    新增产品分类列表页面

--- WebApplication/Controllers/ProductClassController.cs
+++ WebApplication/Controllers/ProductClassController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CSLib;
+using CSLib.BackEnd.JSon;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +15,36 @@ namespace WebApplication.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public String GetListData()
+        {
+            String lResult = "";
+            String lSQL = "";
+            DataTable lDT = null;
+
+            lSQL += "DECLARE";
+            lSQL += "    @RowCnt INT";
+            lSQL += "    SELECT";
+            lSQL += "           @RowCnt=COUNT(*)";
+            lSQL += "    FROM";
+            lSQL += "           T_Product_Class";
+            lSQL += "    WHERE";
+            lSQL += "           DEL='0'";
+            lSQL += "    SELECT";
+            lSQL += "             @RowCnt                       RowCnt";
+            lSQL += "           , ROW_NUMBER()OVER(ORDER BY ID) SerNO";
+            lSQL += "           , Title";
+            lSQL += "    FROM";
+            lSQL += "             T_Product_Class";
+            lSQL += "    WHERE";
+            lSQL += "             DEL='0'";
+
+            if(DBHelper.GetDataTable(lSQL, ref lDT)== EXESQLRET.SUCCESS)
+            {
+                lResult = ConvertToJson.FromDataTable(lDT);
+            }
+            return lResult;
         }
     }
 }
